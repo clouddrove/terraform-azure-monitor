7f3189f1ab068d1c6ca394ce796752463718a305
Anmol Nagpal <ianmolnagpal@gmail.com>
2023-04-13 23:13:16 +0530

    [Release-identos - 177] Initial Commit

---
+++ _example/default/example.tf
@@ -0,0 +1,123 @@
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+module "resource_group" {
+  source  = "clouddrove/resource-group/azure"
+  version = "1.0.2"
+
+  name        = "app-ampls"
+  environment = "test"
+  label_order = ["name", "environment", ]
+  location    = "Canada Central"
+}
+
+module "vnet" {
+  source  = "clouddrove/vnet/azure"
+  version = "1.0.1"
+
+  name                = "app"
+  environment         = "test"
+  label_order         = ["name", "environment"]
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+  address_space       = "10.30.0.0/16"
+}
+
+module "subnet" {
+  source  = "clouddrove/subnet/azure"
+  version = "1.0.2"
+
+  name                 = "app"
+  environment          = "test"
+  label_order          = ["name", "environment"]
+  resource_group_name  = module.resource_group.resource_group_name
+  location             = module.resource_group.resource_group_location
+  virtual_network_name = join("", module.vnet.vnet_name)
+
+  #subnet
+  subnet_names    = ["subnet1"]
+  subnet_prefixes = ["10.30.1.0/24"]
+
+  # route_table
+  routes = [
+    {
+      name           = "rt-test"
+      address_prefix = "0.0.0.0/0"
+      next_hop_type  = "Internet"
+    }
+  ]
+}
+
+module "log-analytics" {
+  source                           = "clouddrove/log-analytics/azure"
+  version                          = "1.0.1"
+  name                             = "app"
+  environment                      = "test"
+  label_order                      = ["name", "environment"]
+  create_log_analytics_workspace   = true
+  log_analytics_workspace_sku      = "PerGB2018"
+  resource_group_name              = module.resource_group.resource_group_name
+  log_analytics_workspace_location = module.resource_group.resource_group_location
+}
+
+module "ampls" {
+  source      = "../"
+  name        = "app"
+  environment = "test"
+  label_order = ["name", "environment"]
+
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+  linked_resource_ids = [module.log-analytics.workspace_id]
+  subnet_id           = module.subnet.default_subnet_id[0]
+  private_dns_zones_names = [
+    "privatelink.agentsvc.azure-automation.net",
+    "privatelink.blob.core.windows.net",
+    "privatelink.monitor.azure.com",
+    "privatelink.ods.opinsights.azure.com",
+    "privatelink.oms.opinsights.azure.com",
+  ]
+}
+
+data "azurerm_subscription" "current" {
+}
+
+
+provider "azurerm" {
+  alias = "other"
+  features {}
+  subscription_id = "82d2a91c-9e70-40c9-8a97-3c1e353b2a80"
+}
+
+data "azurerm_resource_group" "other_rg" {
+  provider = azurerm.other
+  name     = "test"
+}
+
+module "ampls_diff_subs" {
+  source      = "../"
+  name        = "app-1"
+  environment = "test-1"
+  label_order = ["name", "environment"]
+
+  diff_sub_resource_group_name = data.azurerm_resource_group.other_rg.name
+  diff_sub_location            = module.resource_group.resource_group_location
+  subnet_id                     = "/subscriptions/82d2a91c-9e70-40c9-8a97-3c1e353b2a80/resourceGroups/test/providers/Microsoft.Network/virtualNetworks/vnet-test/subnets/default"
+
+  azurerm_monitor_private_link_scope_id = "/subscriptions/cbaecd6a-2e7c-4524-bef7-eb0d2fba93db/resourceGroups/app-ampls-test-resource-group/providers/microsoft.insights/privateLinkScopes/app-test-ampls"
+  diff_sub                              = true
+  alias_sub                             = "82d2a91c-9e70-40c9-8a97-3c1e353b2a80"
+  private_dns_zones_names = [
+    "privatelink.agentsvc.azure-automation.net",
+    "privatelink.blob.core.windows.net",
+    "privatelink.monitor.azure.com",
+    "privatelink.ods.opinsights.azure.com",
+    "privatelink.oms.opinsights.azure.com",
+  ]
+}
+
---
+++ variables.tf
@@ -0,0 +1,140 @@
+#Module      : LABEL
+#Description : Terraform label module variables.
+
+variable "name" {
+  type        = string
+  default     = ""
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "repository" {
+  type        = string
+  default     = "https://github.com/clouddrove/terraform-azure-subnet.git"
+  description = "Terraform current module repo"
+
+  validation {
+    # regex(...) fails if it cannot find a match
+    condition     = can(regex("^https://", var.repository))
+    error_message = "The module-repo value must be a valid Git repo link."
+  }
+}
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "Environment (e.g. `prod`, `dev`, `staging`)."
+}
+
+variable "label_order" {
+  type        = list(any)
+  default     = []
+  description = "Label order, e.g. `name`,`application`."
+}
+
+variable "attributes" {
+  type        = list(any)
+  default     = []
+  description = "Additional attributes (e.g. `1`)."
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `organization`, `environment`, `name` and `attributes`."
+}
+
+variable "tags" {
+  type        = map(any)
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)."
+}
+
+variable "managedby" {
+  type        = string
+  default     = "hello@clouddrove.com"
+  description = "ManagedBy, eg 'CloudDrove'."
+}
+
+variable "enabled" {
+  type        = bool
+  description = "Set to false to prevent the module from creating any resources."
+  default     = true
+}
+
+variable "resource_group_name" {
+  default     = ""
+  description = "A container that holds related resources for an Azure solution"
+}
+
+variable "location" {
+  type        = string
+  default     = ""
+  description = "Location where resource should be created."
+}
+
+variable "ampls_enabled" {
+  type        = bool
+  description = "Set to false to prevent the module from creating ampls resource."
+  default     = true
+}
+
+variable "linked_resource_ids" {
+  type = list(string)
+  default = [ "" ]
+  description = "(Required) The name of the Azure Monitor Private Link Scoped Service. Changing this forces a new resource to be created."
+}
+
+variable "subnet_id" {
+  type = string 
+  default = ""
+  description = "(Required) The ID of the Subnet from which Private IP Addresses will be allocated for this Private Endpoint. Changing this forces a new resource to be created."
+}
+
+variable "subscription_id" {
+  type = string
+  default = ""
+  description = "(Optional) Specifies the ID of the subscription. If this argument is omitted, the subscription ID of the current Azure Resource Manager provider is used."
+}
+
+variable "private_dns_zones_names" {
+  type = list(string)
+  default = [""]
+  description = "The name of the private dns zones from which private dns will be created for AMPLS"
+}
+
+variable "enable_private_endpoint" {
+  type        = bool
+  default     = true
+  description = "enable or disable private endpoint to storage account"
+}
+
+variable "azurerm_monitor_private_link_scope_id" {
+  type = string 
+  default = null 
+  description = "The id  of the monitor private link scope from which private dns will be created for it"
+}
+
+variable "diff_sub" {
+  # To be set true when hosted DNS zone is in different subnscription.
+  type        = bool
+  default     = false
+  description = "Flag to tell whether dns zone is in different sub or not."
+}
+
+variable "alias_sub" {
+  # To be set when you are using a DNS zone from different subscription.
+  type        = string
+  default     = null
+  description = "Different subscription id for local provider(id of diff sub in which DNS zone is present)."
+}
+
+variable "diff_sub_resource_group_name" {
+  default     = ""
+  description = "A container that holds related different subscription resources for an Azure solution"
+}
+
+variable "diff_sub_location" {
+  type        = string
+  default     = ""
+  description = "Location where different subscription resource should be created."
+}
