cd76aeaf60d7f8055dc18614111399c573e557ee
Anmol Nagpal <anmol.nagpal@porall.com>
2023-04-14 13:00:22 +0530

    fix/module-structure

--- _example/default/example.tf
+++ _example/default/example.tf
@@ -84,40 +84,5 @@ module "ampls" {
   ]
 }
 
-data "azurerm_subscription" "current" {
-}
 
-
-provider "azurerm" {
-  alias = "other"
-  features {}
-  subscription_id = "82d2a91c-9e70-40c9-8a97-3c1e353b2a80"
-}
-
-data "azurerm_resource_group" "other_rg" {
-  provider = azurerm.other
-  name     = "test"
-}
-
-module "ampls_diff_subs" {
-  source      = "../../"
-  name        = "app-1"
-  environment = "test-1"
-  label_order = ["name", "environment"]
-
-  diff_sub_resource_group_name = data.azurerm_resource_group.other_rg.name
-  diff_sub_location            = module.resource_group.resource_group_location
-  subnet_id                    = "/subscriptions/82d2a91c-9e70-40c9-8a97-3c1e353b2a80/resourceGroups/test/providers/Microsoft.Network/virtualNetworks/vnet-test/subnets/default"
-
-  azurerm_monitor_private_link_scope_id = "/subscriptions/cbaecd6a-2e7c-4524-bef7-eb0d2fba93db/resourceGroups/app-ampls-test-resource-group/providers/microsoft.insights/privateLinkScopes/app-test-ampls"
-  diff_sub                              = true
-  alias_sub                             = "82d2a91c-9e70-40c9-8a97-3c1e353b2a80"
-  private_dns_zones_names = [
-    "privatelink.agentsvc.azure-automation.net",
-    "privatelink.blob.core.windows.net",
-    "privatelink.monitor.azure.com",
-    "privatelink.ods.opinsights.azure.com",
-    "privatelink.oms.opinsights.azure.com",
-  ]
-}
 
